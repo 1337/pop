4c96ec670218d2714a08730c84e4cbd8ea626d68
Brian Lai <b2lai@uwaterloo.ca>
2013-10-23 22:07:47 -0400

    Add filter conditions

--- modules/Collection.php
+++ modules/Collection.php
@@ -43,16 +43,59 @@
                 $key_comp = $decomposition[1];  // e.g. eq
 
                 foreach ((array) $matches as $idx => $model) {
-                    if ($model->$key_name === null) {
+                    $model_key_val = $model->$key_name;
+                    if ($model_key_val === null) {
                         // unless the query is actually looking for == null,
                         // this is a failed model for not having that attribute
                         if ($key_comp === 'eq' && $value === null) {
-
+                            // well fine
+                            break;
                         } else {
                             // no such attribute, waste no time
                             unset($matches[$idx]);
                         }
                     }
+                    switch ($key_comp) {
+                        case 'eq':
+                            if ($model_key_val === $value) {
+                                break 2;  // 2? http://www.php.net/break
+                            }
+                            break;
+                        case 'lt':
+                            if ($model_key_val < $value) {
+                                break 2;  // 2? http://www.php.net/break
+                            }
+                            break;
+                        case 'lte':
+                            if ($model_key_val <= $value) {
+                                break 2;  // 2? http://www.php.net/break
+                            }
+                            break;
+                        case 'gt':
+                            if ($model_key_val > $value) {
+                                break 2;  // 2? http://www.php.net/break
+                            }
+                            break;
+                        case 'gte':
+                            if ($model_key_val >= $value) {
+                                break 2;  // 2? http://www.php.net/break
+                            }
+                            break;
+                        case 'in':
+                            // '5' in [1,2,3,4,5]
+                            if (in_array($model_key_val, $value)) {
+                                break 2;  // 2? http://www.php.net/break
+                            }
+                            break;
+                        case 'contains':
+                            // [1,2,3,4,5] contains '5'
+                            if (in_array($value, $model_key_val)) {
+                                break 2;  // 2? http://www.php.net/break
+                            }
+                            break;
+                    }
+                    // if nothing passes, reject the model
+                    unset($matches[$idx]);
                 }
             }
             return $matches;
