fbf4244b62dfca474d34b848068c0a1ebacef25a
Brian Lai <b2lai@uwaterloo.ca>
2013-10-23 21:57:09 -0400

    Add filter

--- modules/Collection.php
+++ modules/Collection.php
@@ -8,7 +8,7 @@
         public $models = array();
 
         /**
-         * @param {Array} $models
+         * @param Array $models
          */
         public function __construct($models) {
             $this->models = $models;
@@ -21,7 +21,40 @@
             return sizeof($this->models);
         }
 
-        public function filter() {
+        /**
+         * @param array $filters  something in the format {"age__eq": "50"}
+         * @return array          models that pass all these filters.
+         * @throws UnexpectedValueException
+         */
+        public function filter($filters) {
+            $matches = $this->models;
 
+            if (!is_array($filters)) {
+                throw new UnexpectedValueException('Filters must be array');
+            }
+            foreach($filters as $condition => $value) {
+                if (strpos($condition, '__') === false) {
+                    // format foo, assuming foo__eq
+                    $condition = $condition . '__eq';
+                }
+                // format foo__bar
+                $decomposition = explode('__', $condition);
+                $key_name = $decomposition[0];  // e.g. pk
+                $key_comp = $decomposition[1];  // e.g. eq
+
+                foreach ((array) $matches as $idx => $model) {
+                    if ($model->$key_name === null) {
+                        // unless the query is actually looking for == null,
+                        // this is a failed model for not having that attribute
+                        if ($key_comp === 'eq' && $value === null) {
+
+                        } else {
+                            // no such attribute, waste no time
+                            unset($matches[$idx]);
+                        }
+                    }
+                }
+            }
+            return $matches;
         }
     }
